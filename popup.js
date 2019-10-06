7d94221aca15a30ef7d739b64e8a97a4604d0c09
atallah-salah <atallah_salah@outlook.com>
2019-10-06 12:07:08 +0300

    style popup html

--- popup.js
+++ popup.js
@@ -1,10 +1,26 @@
+let status ="idle";
 
 
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, {type:"getText"}, function(response){
-      // alert(response)
-      // $("#text").text(response);
-      console.log(5555,response);
-      
+document.getElementById("startRecord").addEventListener("click",()=>{
+  startRecord();
+})
+
+
+
+function startRecord() {
+  console.log("test");
+  
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {type:"startRecord"}, function(response){
+        console.log("popup js :res :startRecord",response);
+    });
   });
-});
+}
+
+function stopRecord() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {type:"stopRecord"}, function(response){
+        console.log("popup js :res :stopRecord",response);
+    });
+  });
+}
