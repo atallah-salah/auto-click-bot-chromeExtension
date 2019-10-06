37cd55782468fbb9c06ac7cf04deea225a394dcf
atallah-salah <atallah_salah@outlook.com>
2019-10-06 12:43:29 +0300

    recive send message

---
+++ content.js
@@ -0,0 +1,16 @@
+let clicksArray=[];
+
+
+window.addEventListener('click',(item)=>{
+  clicksArray.push({id:clicksArray.length,x:item.pageX,y:item.pageY,time:"0"})
+  console.log('test',clicksArray);
+})
+
+chrome.runtime.onMessage.addListener(gotMessage)
+
+function gotMessage(req,sender,res) {
+  console.log("new message",req,sender,res);
+  res("test");
+  return true;
+  
+}
--- popup.js
+++ popup.js
@@ -1,15 +1,42 @@
 let status ="idle";
 
+// check status
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {type:"status"}, function(response){
+      console.log("popup js :res :status",response);
+      // status = response.status;
+  });
+});
 
-document.getElementById("startRecord").addEventListener("click",()=>{
-  startRecord();
+
+
+firstButton = document.getElementById("startRecord");
+
+firstButton.addEventListener("click",()=>{
+  switchButton();
 })
 
 
-
-function startRecord() {
-  console.log("test");
+function switchButton(){  
+  switch (status) {
+    case "idle":
+      status = "recording";      
+      firstButton.textContent="recording";
+      startRecord();
+      break;
   
+      case "recording":
+      status = "idle";      
+      firstButton.textContent="Start recroding";
+      stopRecord();
+      break;
+  }
+}
+
+
+
+
+function startRecord() {  
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {type:"startRecord"}, function(response){
         console.log("popup js :res :startRecord",response);
